166a8234fc05c9d00364512b248b9d31c6cc2450
Takashi-Kanesawa <takashi.kanesawa@aimnext.com>
2024-12-05 17:47:37 +0900

    ComDispatchProxyにIDisposableを実装し、COMオブジェクトを解放する

--- ExcelSample/ComDispatchProxy.cs
+++ ExcelSample/ComDispatchProxy.cs
@@ -1,13 +1,20 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using Excel = Microsoft.Office.Interop.Excel;
 
-public class ComDispatchProxy<T> : DispatchProxy where T : class 
+public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class 
 {
     private T _comObject;
+    private T _validProxy;
 
-    public void Initialize(T comObject)
+    public T Proxy => this._validProxy;
+
+    public void Initialize(ComDispatchProxy<T> creatingProxy, T comObject)
     {
         this._comObject = comObject;
+
+        this._validProxy = creatingProxy as T;
     }
 
     protected override object? Invoke(MethodInfo? method, object?[]? args)
@@ -15,15 +22,21 @@ public class ComDispatchProxy<T> : DispatchProxy where T : class
         return method.Invoke(_comObject, args);
     }
 
-    public static T CreateProxy(T comObject)
+    public static ComDispatchProxy<T> CreateProxy(T comObject)
     {
         var proxy = Create<T, ComDispatchProxy<T>>() as ComDispatchProxy<T>;
-        proxy.Initialize(comObject);
+        proxy.Initialize(proxy, comObject);
 
-        return proxy as T;
+        return proxy;
     }
 
-    ~ComDispatchProxy()
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this); // デストラクタをスキップする
+    }
+
+    protected virtual void Dispose(bool disposing)
     {
         if (_comObject != null && Marshal.IsComObject(_comObject))
         {
@@ -31,6 +44,12 @@ public class ComDispatchProxy<T> : DispatchProxy where T : class
             Marshal.ReleaseComObject(_comObject);
 #pragma warning restore CA1416
         }
+
+    }
+
+    ~ComDispatchProxy()
+    {
+        Dispose(false); // ファイナライザから呼び出される場合
     }
 
 }
--- ExcelSample/Program.cs
+++ ExcelSample/Program.cs
@@ -1,25 +1,25 @@
 ﻿using Excel = Microsoft.Office.Interop.Excel;
 
-var excelApp = ComDispatchProxy<Excel.Application>.CreateProxy(new Excel.Application());
-var wbs = ComDispatchProxy<Excel.Workbooks>.CreateProxy(excelApp?.Workbooks);
-var wb = ComDispatchProxy<Excel.Workbook>.CreateProxy(wbs?.Add());
-var wss = ComDispatchProxy<Excel.Sheets>.CreateProxy(wb?.Sheets);
-var ws = ComDispatchProxy<Excel.Worksheet>.CreateProxy(wss?[1] as Excel.Worksheet);
-var targetRange = ComDispatchProxy<Excel.Range>.CreateProxy(ws?.Range["A1:A10"]);
-var columns = ComDispatchProxy<Excel.Range>.CreateProxy(targetRange?.Columns);
+using (var excelApp = ComDispatchProxy<Excel.Application>.CreateProxy(new Excel.Application()))
+using (var wbs = ComDispatchProxy<Excel.Workbooks>.CreateProxy(excelApp?.Proxy.Workbooks))
+using (var wb = ComDispatchProxy<Excel.Workbook>.CreateProxy(wbs?.Proxy.Add()))
+using (var wss = ComDispatchProxy<Excel.Sheets>.CreateProxy(wb?.Proxy.Sheets))
+using (var ws = ComDispatchProxy<Excel.Worksheet>.CreateProxy(wss?.Proxy[1] as Excel.Worksheet))
+using (var targetRange = ComDispatchProxy<Excel.Range>.CreateProxy(ws?.Proxy.Range["A1:A10"]))
+using (var columns = ComDispatchProxy<Excel.Range>.CreateProxy(targetRange?.Proxy.Columns))
 
 if (excelApp is not null && targetRange is not null && columns is not null)
 {
-    excelApp.Visible = true;                      // Excelを可視化（見せない方が実は速い）
-    var startDate = DateTime.Today;               // 今日から10日分の日付データ作成
+    excelApp.Proxy.Visible = true;                      // Excelを可視化（見せない方が実は速い）
+    var startDate = DateTime.Today;                     // 今日から10日分の日付データ作成
     var dates = new DateTime[10];
     for (int i = 0; i < 10; i++)
     {
         dates[i] = startDate.AddDays(i);
     }
 
-    targetRange.NumberFormat = "yyyy年mm月dd日";  // 編集範囲の書式指定
-    targetRange.Value = dates;                    // 編集範囲にまとめて入力（一個ずつやるとめちゃ遅い）
-    columns.AutoFit();                // 列幅を自動調整
+    targetRange.Proxy.NumberFormat = "yyyy年mm月dd日";  // 編集範囲の書式指定
+    targetRange.Proxy.Value = dates;                    // 編集範囲にまとめて入力（一個ずつやるとめちゃ遅い）
+    columns.Proxy.AutoFit();                            // 列幅を自動調整
 }
 
