e4a712bd10efc27511defa97b9923a7a04cca7e8
Takashi-Kanesawa <takashi.kanesawa@aimnext.com>
2024-12-05 16:32:17 +0900

    ComDispatchProxyにデストラクタを実装

--- ExcelSample/ComDispatchProxy.cs
+++ ExcelSample/ComDispatchProxy.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 public class ComDispatchProxy<T> : DispatchProxy where T : class 
 {
     private T _comObject;
-    
+
     public void Initialize(T comObject)
     {
         this._comObject = comObject;
@@ -22,5 +22,16 @@ public class ComDispatchProxy<T> : DispatchProxy where T : class
 
         return proxy as T;
     }
+
+    ~ComDispatchProxy()
+    {
+        if (_comObject != null && Marshal.IsComObject(_comObject))
+        {
+#pragma warning disable CA1416 // OS プラットフォームの互換性の警告を無視
+            Marshal.ReleaseComObject(_comObject);
+#pragma warning restore CA1416
+        }
+    }
+
 }
 
