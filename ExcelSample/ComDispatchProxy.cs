2e99124dfc339f6707ff9250ab9cdec4742897a0
Takashi-Kanesawa <takashi.kanesawa@aimnext.com>
2024-12-05 16:08:40 +0900

    最低限で動作する範囲のDispatchProxyを実装。

---
+++ ExcelSample/ComDispatchProxy.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+public class ComDispatchProxy<T> : DispatchProxy where T : class 
+{
+    private T _comObject;
+    
+    public void Initialize(T comObject)
+    {
+        this._comObject = comObject;
+    }
+
+    protected override object? Invoke(MethodInfo? method, object?[]? args)
+    {
+        return method.Invoke(_comObject, args);
+    }
+
+    public static T CreateProxy(T comObject)
+    {
+        var proxy = Create<T, ComDispatchProxy<T>>() as ComDispatchProxy<T>;
+        proxy.Initialize(comObject);
+
+        return proxy as T;
+    }
+}
+
--- ExcelSample/Program.cs
+++ ExcelSample/Program.cs
@@ -1,39 +1,25 @@
-﻿using System.Runtime.InteropServices;
+﻿using Excel = Microsoft.Office.Interop.Excel;
 
-using Excel = Microsoft.Office.Interop.Excel;
-var excelApp = new Excel.Application();                 // Excel起動
-var wbs = excelApp?.Workbooks;
-var wb = wbs.Add();                                     // Workbook取得
-var wss = wb?.Sheets;
-var ws = wss[1] as Excel.Worksheet;                     // Worksheet取得
-var targetRange = ws?.Range["A1:A10"] as Excel.Range;   // 編集範囲のRangeオブジェクトを取得
-var columns = targetRange?.Columns as Excel.Range;
-if (excelApp is not null && wb is not null && ws is not null && targetRange is not null)
+var excelApp = ComDispatchProxy<Excel.Application>.CreateProxy(new Excel.Application());
+var wbs = ComDispatchProxy<Excel.Workbooks>.CreateProxy(excelApp?.Workbooks);
+var wb = ComDispatchProxy<Excel.Workbook>.CreateProxy(wbs?.Add());
+var wss = ComDispatchProxy<Excel.Sheets>.CreateProxy(wb?.Sheets);
+var ws = ComDispatchProxy<Excel.Worksheet>.CreateProxy(wss?[1] as Excel.Worksheet);
+var targetRange = ComDispatchProxy<Excel.Range>.CreateProxy(ws?.Range["A1:A10"]);
+var columns = ComDispatchProxy<Excel.Range>.CreateProxy(targetRange?.Columns);
+
+if (excelApp is not null && targetRange is not null && columns is not null)
 {
-    try
+    excelApp.Visible = true;                      // Excelを可視化（見せない方が実は速い）
+    var startDate = DateTime.Today;               // 今日から10日分の日付データ作成
+    var dates = new DateTime[10];
+    for (int i = 0; i < 10; i++)
     {
-        excelApp.Visible = true;                        // Excelを可視化（見せない方が実は速い）
-        var startDate = DateTime.Today;                 // 今日から10日分の日付データ作成
-        var dates = new DateTime[10];
-        for (int i = 0; i < 10; i++)
-        {
-            dates[i] = startDate.AddDays(i);
-        }
-        targetRange.NumberFormat = "yyyy年mm月dd日";    // 編集範囲の書式指定
-        targetRange.Value = dates;                      // 編集範囲にまとめて入力
-        columns.AutoFit();                              // 列幅を自動調整
+        dates[i] = startDate.AddDays(i);
     }
-    finally
-    {
-        // プラットフォーム固有のメソッド(Marshal.ReleaseComObject)を使うと警告が出るので抑止
-#pragma warning disable CA1416
-        Marshal.ReleaseComObject(columns);             // COMオブジェクトの解放
-        Marshal.ReleaseComObject(targetRange);
-        Marshal.ReleaseComObject(ws);
-        Marshal.ReleaseComObject(wss);
-        Marshal.ReleaseComObject(wb);
-        Marshal.ReleaseComObject(wbs);
-        Marshal.ReleaseComObject(excelApp);
-#pragma warning restore CA1416
-    }
+
+    targetRange.NumberFormat = "yyyy年mm月dd日";  // 編集範囲の書式指定
+    targetRange.Value = dates;                    // 編集範囲にまとめて入力（一個ずつやるとめちゃ遅い）
+    columns.AutoFit();                // 列幅を自動調整
 }
+
