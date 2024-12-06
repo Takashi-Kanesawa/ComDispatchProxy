97a061b2193b0a44323fd86d3921b0cd5f78dada
Takashi-Kanesawa <takashi.kanesawa@aimnext.com>
2024-12-06 15:59:35 +0900

    暗黙的なCOMオブジェクトの取得をアプリ側に埋め込む

--- ExcelSample/Program.cs
+++ ExcelSample/Program.cs
@@ -1,14 +1,11 @@
 ﻿using Excel = Microsoft.Office.Interop.Excel;
 
 using (var excelApp = ComDispatchProxy<Excel.Application>.CreateProxy(new Excel.Application()))
-using (var wbs = excelApp?.Proxy.Workbooks as ComDispatchProxy<Excel.Workbooks>)
-using (var wb = wbs?.Proxy.Add() as ComDispatchProxy<Excel.Workbook>)
-using (var wss = wb?.Proxy.Sheets as ComDispatchProxy<Excel.Sheets>)
-using (var ws = wss?.Proxy[1] as ComDispatchProxy<Excel.Worksheet>)
+using (var wb =  excelApp?.Proxy.Workbooks.Add() as ComDispatchProxy<Excel.Workbook>)
+using (var ws =  wb?.Proxy.Sheets[1] as ComDispatchProxy<Excel.Worksheet>)
 using (var targetRange = ws?.Proxy.Range["A1:A10"] as ComDispatchProxy<Excel.Range>)
-using (var columns = targetRange?.Proxy.Columns as ComDispatchProxy<Excel.Range>)
 
-if (excelApp is not null && targetRange is not null && columns is not null)
+if (excelApp is not null && targetRange is not null)
 {
     excelApp.Proxy.Visible = true;                      // Excelを可視化（見せない方が実は速い）
     var startDate = DateTime.Today;                     // 今日から10日分の日付データ作成
@@ -20,6 +17,6 @@ if (excelApp is not null && targetRange is not null && columns is not null)
 
     targetRange.Proxy.NumberFormat = "yyyy年mm月dd日";  // 編集範囲の書式指定
     targetRange.Proxy.Value = dates;                    // 編集範囲にまとめて入力（一個ずつやるとめちゃ遅い）
-    columns.Proxy.AutoFit();                            // 列幅を自動調整
+    targetRange.Proxy.Columns.AutoFit();                // 列幅を自動調整
 }
 
