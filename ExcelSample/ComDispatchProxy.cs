d16bdccf13b41e1ed57e880bd4d0a1a2731b807f
Takashi-Kanesawa <takashi.kanesawa@aimnext.com>
2025-01-23 10:52:49 +0900

    対応するMicrosoft.Office.Interop.Excelのインターフェイスを追加。ログ出力をConsoleからDebugに変更。

--- ExcelSample/ComDispatchProxy.cs
+++ ExcelSample/ComDispatchProxy.cs
@@ -1,13 +1,25 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using Excel = Microsoft.Office.Interop.Excel;
 
 /// <summary>
+/// 生のCOMオブジェクトを取得するためのインターフェイス
+/// </summary>
+public interface IGetRowObject
+{
+    /// <summary>
+    /// 生のCOMオブジェクトを取得します
+    /// </summary>
+    object RowObject {  get; }
+}
+
+/// <summary>
 /// COMオブジェクトのプロキシを作成し、メソッド呼び出しを中継するためのクラス。
 /// </summary>
 /// <typeparam name="T">プロキシ化するCOMオブジェクトの型。</typeparam>
-public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
+public class ComDispatchProxy<T> : DispatchProxy, IGetRowObject, IDisposable where T : class
 {
     // 現在のプロキシインスタンスとそれに関連する情報を保持する辞書
     private static readonly Dictionary<ComDispatchProxy<T>, string> _instances = new();
@@ -22,6 +34,8 @@ public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
     /// </summary>
     public T Proxy { get => this._validProxy.Value; }
 
+    public object RowObject => this._comObject.Value;
+
     /// <summary>
     /// コンストラクタ。プロキシの初期化処理を行います。
     /// </summary>
@@ -30,21 +44,6 @@ public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
         lock (_instances)
         {
             _instances.Add(this, filterStackTrace(Environment.StackTrace));
-        }
-
-        // スタックトレースのフィルタリングメソッド
-        string filterStackTrace(string stackTrace)
-        {
-            Regex[] includesRegex = { new Regex(@"cs:line \d+$") };
-
-            // スタックトレースの行ごとにフィルタリング
-            var filteredStackTrace = string.Join(Environment.NewLine, stackTrace
-                .Split(new[] { Environment.NewLine }, StringSplitOptions.None)
-                .Where(line =>
-                    includesRegex.Any(regex => regex.IsMatch(line)) &&
-                    line.StartsWith("   at ComDispatchProxy`") == false));
-
-            return filteredStackTrace;
         }
 
         // アプリケーション終了時の未解放オブジェクトを警告
@@ -56,10 +55,10 @@ public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
         // 未処理例外発生時のハンドリング
         AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
         {
-            Console.WriteLine("[ERROR] Unhandled exception occurred.");
+            Debug.WriteLine("[ERROR] Unhandled exception occurred.");
             if (e.ExceptionObject is Exception ex)
             {
-                Console.WriteLine($"Exception details: {ex}");
+                Debug.WriteLine($"Exception details: {ex}");
             }
             HandleApplicationExit("UnhandledException");
         };
@@ -69,11 +68,11 @@ public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
         {
             if (_instances.Count > 0)
             {
-                Console.WriteLine($"[WARNING] Application exiting due to {reason}. Some COM objects were not released properly:");
+                Debug.WriteLine($"[WARNING] Application exiting due to {reason}. Some COM objects were not released properly:");
                 foreach (var instanceInfo in _instances)
                 {
-                    Console.WriteLine($" - {instanceInfo.Key._objectName}");
-                    Console.WriteLine($"{instanceInfo.Value}");
+                    Debug.WriteLine($" - {instanceInfo.Key._objectName}");
+                    Debug.WriteLine($"{instanceInfo.Value}");
                     instanceInfo.Key.Dispose();
                 }
             }
@@ -84,6 +83,23 @@ public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
         this._objectName = new Lazy<string>(() => throw new InvalidOperationException("Object name is not initialized."));
         this._validProxy = new Lazy<T>(() => throw new InvalidOperationException("COM Object is not initialized."));
     }
+
+
+    // スタックトレースのフィルタリングメソッド
+    private static string filterStackTrace(string stackTrace)
+    {
+        Regex[] includesRegex = { new Regex(@"cs:line \d+$") };
+
+        // スタックトレースの行ごとにフィルタリング
+        var filteredStackTrace = string.Join(Environment.NewLine, stackTrace
+            .Split(new[] { Environment.NewLine }, StringSplitOptions.None)
+            .Where(line =>
+                includesRegex.Any(regex => regex.IsMatch(line)) &&
+                line.StartsWith("   at ComDispatchProxy`") == false));
+
+        return filteredStackTrace;
+    }
+
 
     /// <summary>
     /// プロキシを初期化します。
@@ -115,7 +131,10 @@ public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
         if (_comObject.Value == null)
             throw new InvalidOperationException("COM Object is not initialized.");
 
-        Console.WriteLine($"[LOG] Invoking '{method.Name}' on {_objectName.Value} with args: {FormatArgs(args)}");
+        // 引数リスト内のラッパーを剥がす
+        UnwrapPoxiesInArgs(args);
+
+        Debug.WriteLine($"[LOG] Invoking '{method.Name}' on {_objectName.Value} with args: {FormatArgs(args)}");
 
         try
         {
@@ -124,11 +143,11 @@ public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
 
             if (result == null)
             {
-                Console.WriteLine($"[LOG] Method '{method.Name}' returned null.");
+                Debug.WriteLine($"[LOG] Method '{method.Name}' returned null.");
                 return null;
             }
 
-            // COMオブジェクトの場合、適切なプロキシを生成
+            // COM オブジェクトの場合、適切なプロキシを生成
             if (Marshal.IsComObject(result))
             {
                 return ProxyFactory(method, result);
@@ -138,21 +157,37 @@ public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
         }
         catch (TargetInvocationException ex)
         {
-            Console.WriteLine($"[ERROR] Exception in '{method.Name}': {ex.InnerException?.Message}");
+            Debug.WriteLine($"[ERROR] Exception in '{method.Name}': {ex.InnerException?.Message}");
             throw ex.InnerException ?? ex;
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"[ERROR] Exception in '{method.Name}': {ex.Message}");
+            Debug.WriteLine($"[ERROR] Exception in '{method.Name}': {ex.Message}");
             throw;
         }
 
-        // 引数のフォーマット処理
+        // ローカル関数：引数のフォーマット処理
         string FormatArgs(object?[]? args)
         {
             if (args == null || args.Length == 0)
                 return "none";
             return string.Join(", ", args.Select(arg => arg?.ToString() ?? "null"));
+        }
+
+        // ローカル関数：引数のCOMオブジェクトをDispatchProxyから生のCOMオブジェクトに置換
+        void UnwrapPoxiesInArgs(object?[]? args)
+        {
+            if (args == null)
+                return;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] is not IGetRowObject proxy)
+                    continue;
+
+                // IGetRowObject を実装している場合は RowObject と入れ替える
+                args[i] = proxy.RowObject;
+            }
         }
     }
 
@@ -166,25 +201,80 @@ public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
             throw new ArgumentNullException($"{nameof(method)} or {nameof(result)} is null.");
         }
 
+        Debug.WriteLine($"result.GetType() = {result.GetType()}");
+
         // 型ごとにプロキシを生成
         switch (result)
         {
-            case Excel.Application app: return WrapProxy(app);
-            case Excel.Workbooks wbs: return WrapProxy(wbs);
-            case Excel.Workbook wb: return WrapProxy(wb);
-            case Excel.Sheets wss: return WrapProxy(wss);
-            case Excel.Worksheet ws: return WrapProxy(ws);
-            case Excel.Range range: return WrapProxy(range);
-            case Excel.Windows wins: return WrapProxy(wins);
-            case Excel.Window win: return WrapProxy(win);
-        }
+            // 基本操作で頻繁に使用されるオブジェクト
+            case Excel.Range range: return WrapProxy(range);    // Rangeは最も使用頻度が高そう
+            case Excel.Application application: return WrapProxy(application);
+            case Excel.Workbooks workbooks: return WrapProxy(workbooks);
+            case Excel.Workbook workbook: return WrapProxy(workbook);
+            case Excel.Sheets sheets: return WrapProxy(sheets);
+            case Excel.Worksheet worksheet: return WrapProxy(worksheet);
 
-        Console.WriteLine($"[LOG] Can't create proxy for {_objectName.Value}.{method.Name}");
+            // スタイル・グラフ関連の末端オブジェクト
+            case Excel.Font font: return WrapProxy(font);
+            case Excel.Border border: return WrapProxy(border);
+            case Excel.Borders borders: return WrapProxy(borders);
+            case Excel.Chart chart: return WrapProxy(chart);
+            case Excel.ChartObject chrObj: return WrapProxy(chrObj);
+            case Excel.Axis axis: return WrapProxy(axis);
+            case Excel.Point point: return WrapProxy(point);
+            case Excel.Series series: return WrapProxy(series);
+
+            // グラフ関連のコレクション
+            case Excel.ChartObjects chrObjs: return WrapProxy(chrObjs);
+            case Excel.SeriesCollection seriesCollction: return WrapProxy(seriesCollction);
+            case Excel.Axes axes: return WrapProxy(axes);
+            case Excel.Points points: return WrapProxy(points);
+
+            // 使用頻度の少なそうな他の要素
+            case Excel.Shape shape: return WrapProxy(shape);
+            case Excel.Shapes shapes: return WrapProxy(shapes);
+            case Excel.ListObject lstObj: return WrapProxy(lstObj);
+            case Excel.ListObjects lstObjs: return WrapProxy(lstObjs);
+            case Excel.Hyperlink hLink: return WrapProxy(hLink);
+            case Excel.Hyperlinks hLinks: return WrapProxy(hLinks);
+            case Excel.Name name: return WrapProxy(name);
+            case Excel.Names names: return WrapProxy(names);
+            case Excel.PivotTable pt: return WrapProxy(pt);
+            case Excel.PivotTables pts: return WrapProxy(pts);
+            case Excel.Interior interior: return WrapProxy(interior);
+            case Excel.Pictures pictures: return WrapProxy(pictures);
+
+            // その他
+            case Excel.Windows windows: return WrapProxy(windows);
+            case Excel.Window window: return WrapProxy(window);
+            case Excel.AutoFilter af: return WrapProxy(af);
+            case Excel.Filters filters: return WrapProxy(filters);
+            case Excel.PageSetup pageSetup: return WrapProxy(pageSetup);
+            case Excel.QueryTable queryTable: return WrapProxy(queryTable);
+            case Excel.TableStyle tableStyle: return WrapProxy(tableStyle);
+            case Excel.PivotCache pivotCache: return WrapProxy(pivotCache);
+            case Excel.PivotField pivotField: return WrapProxy(pivotField);
+            case Excel.PivotItem pivotItem: return WrapProxy(pivotItem);
+            case Excel.Trendlines trendlines: return WrapProxy(trendlines);
+            case Excel.DataLabels dataLabels: return WrapProxy(dataLabels);
+            case Excel.DataLabel dataLabel: return WrapProxy(dataLabel);
+            case Excel.Legend legend: return WrapProxy(legend);
+            case Excel.Style style: return WrapProxy(style);
+            case Excel.Validation validation: return WrapProxy(validation);
+            case Excel.FormatCondition formatCondition: return WrapProxy(formatCondition);
+            case Excel.FormatConditions formatConditions: return WrapProxy(formatConditions);
+            case Excel.Comment comment: return WrapProxy(comment);
+            case Excel.Comments comments: return WrapProxy(comments);
+            case Excel.GroupBoxes groupBoxes: return WrapProxy(groupBoxes);
+        }
+        
+        Debug.WriteLine($"[LOG] Can't create proxy for {_objectName.Value}.{method.Name}");
         return result; // 型が一致しない場合そのまま返す
 
+        // ローカル関数：COMオブジェクトをDispatchProxyでラップする。
         ComDispatchProxy<TCom> WrapProxy<TCom>(TCom comObject) where TCom : class
         {
-            Console.WriteLine($"[LOG] Wrapping returned COM object from '{method.Name}' as '{typeof(TCom)}'.");
+            Debug.WriteLine($"[LOG] Wrapping returned COM object from '{method.Name}' as '{typeof(TCom)}'.");
             return ComDispatchProxy<TCom>.CreateProxy(comObject) as ComDispatchProxy<TCom>;
         }
     }
@@ -207,6 +297,7 @@ public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
         }
 
         proxy.Initialize(proxy, comObject);
+        Debug.WriteLine($"[LOG] ComDispatchProxy is created as '{typeof(T)}' at \n{filterStackTrace(Environment.StackTrace)}");
 
         return proxy;
     }
@@ -222,6 +313,8 @@ public class ComDispatchProxy<T> : DispatchProxy, IDisposable where T : class
 
     protected virtual void Dispose(bool disposing)
     {
+        Debug.WriteLine($"{this._objectName} is disposing.");
+
         lock (_instances)
         {
             _instances.Remove(this);
